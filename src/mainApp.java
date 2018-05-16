4afb23d5b5026618286ff8924fa008e5183c8743
Kubokoo <Kubokoo@users.noreply.github.com>
2018-05-16 22:03:39 +0200

    Reading where wallpaper is stored

--- src/mainApp.java
+++ src/mainApp.java
@@ -1,18 +1,29 @@
 import java.lang.reflect.InvocationTargetException;
+import java.util.Scanner;
 
 public class mainApp {
     public static void main(String[] args) {
-        String value = null;                                              //ValueName
+        String wallpaperStyle= readRegistryKeyString("Control Panel\\Desktop","WallpaperStyle");
+        String wallPaperPath= readRegistryKeyString("Control Panel\\Desktop","WallPaper");
+
+        Scanner scanner = new Scanner(System.in);
+        String newWallPaperPath = scanner.next();
+
+
+    }
+
+    private static String readRegistryKeyString(String key,String keyName){
+        String value=null;
         try {
             value = WinRegistry.readString (
-                    WinRegistry.HKEY_LOCAL_MACHINE,                             //HKEY
-                    "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion",           //Key
-                    "ProductName");
+                    WinRegistry.HKEY_CURRENT_USER,
+                    key,
+                    keyName);
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         } catch (InvocationTargetException e) {
             e.printStackTrace();
         }
-        System.out.println("Windows Distribution = " + value);
+        return(value);
     }
 }
